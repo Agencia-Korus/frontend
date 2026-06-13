f502fde84abfbc8c60b22089a0258d056e88d304
lcsgborges <lcsgborges@gmail.com>
2026-06-13 13:23:06 -0300

    chore: adiciona arquivos de configuracoes em pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
+"use client";
+
+import { Landing } from '@/src/korus/pages/Landing';
+
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>Korus</h1>
-    </div>
-  );
+  return <Landing />;
 }
